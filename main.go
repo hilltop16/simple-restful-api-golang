e6f28fd8373a787d3416125b2b0ef6e8cf0cfa38
Ben Lu <hilltop16@gmail.com>
2018-04-13 22:13:59 +0800

    prepare crud methods

--- main.go
+++ main.go
@@ -8,7 +8,29 @@ import (
 
 func main() {
 	router := mux.NewRouter()
+	router.HandleFunc("/people", getPeopleEndpoint).Methods("GET")
+	router.HandleFunc("/people/{id}", getPersonEndpoint).Methods("GET")
+	router.HandleFunc("/people/{id}", createPersonEndpoint).Methods("post")
+	router.HandleFunc("/people/{id}", deletePersonEndpoint).Methods("delete")
 	log.Fatal(http.ListenAndServe(":3000",router))
 }
+
+func getPeopleEndpoint(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func getPersonEndpoint(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func createPersonEndpoint(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func deletePersonEndpoint(w http.ResponseWriter, req *http.Request) {
+
+}
+
+
 
 
